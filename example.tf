141f54032472fc5c74c1f82f5353acf8e88270c6
kadoyau <el.psi.congroo@gmail.com>
2017-07-23 02:34:06 +0900

    initial commit

---
+++ example.tf
@@ -0,0 +1,21 @@
+# Configure the Heroku provider
+provider "heroku" {
+  email = "${var.heroku_email}"
+  api_key = "${var.heroku_api_key}"
+}
+
+# Create a new application
+resource "heroku_app" "default" {
+  name = "heroku-terraforming-tenarai"
+  region = "us"
+}
+
+resource "heroku_addon" "database" {
+  app  = "${heroku_app.default.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+resource "heroku_addon" "logging" {
+  app  = "${heroku_app.default.name}"
+  plan = "papertrail:choklad"
+}
---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "heroku_email" {}
+# `heroku auth:token` to get your api_key
+variable "heroku_api_key" {}
+variable "REPEAT" {
+  default = 10
+}
